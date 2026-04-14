e972ba513740130366313a18657cd3fb58acad0e
Martin Nash <m-nash@unext.jp>
2026-04-14 22:18:41 +0000

    Created script to test loading data from MultiClinSum dataset folder structure

---
+++ confirm_read_data.py
@@ -0,0 +1,34 @@
+from pathlib import Path
+from datasets import Dataset
+
+def read_training_data():
+    parent = "data/multiclinsum_gs_train_en"
+    yield from read_folder(parent)
+
+def read_folder(root: str):
+    """Reads MultiClinSum data from parent folder"""
+
+    root_path = Path(root)
+    fulltext = root_path / "fulltext"
+    summaries = root_path / "summaries"
+
+    for fulltext_file_path in fulltext.iterdir():
+        if fulltext_file_path.is_file():
+            summary_file_path = summaries / (fulltext_file_path.stem + "_sum.txt")
+
+            with open(fulltext_file_path, 'r+') as f:
+                original_text = f.read()
+
+            with open(summary_file_path, 'r+') as f:
+                summary_text = f.read()
+
+            yield {
+                "input": original_text,
+                "target": summary_text
+            }
+
+if __name__ == "__main__":
+    print("start")
+    ds = Dataset.from_generator(read_training_data)
+    print(ds)
+    print("done")
