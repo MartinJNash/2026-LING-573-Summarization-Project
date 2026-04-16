1372e468c55992e90082df972e696a3f4e0c5b8e
Martin Nash <m-nash@unext.jp>
2026-04-16 15:54:39 +0000

    try to fix bug

--- pipeline.py
+++ pipeline.py
@@ -51,6 +51,7 @@ def main():
 
     def compute_metrics(eval_pred):
         predictions, labels = eval_pred
+        predictions = np.clip(predictions, 0, tokenizer.vocab_size - 1)
         decoded_preds = tokenizer.batch_decode(predictions, skip_special_tokens=True)
         labels = np.where(labels != -100, labels, tokenizer.pad_token_id)
         decoded_labels = tokenizer.batch_decode(labels, skip_special_tokens=True)
