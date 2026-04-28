b0dcd0164acafef2065b5381152d8e866e699e09
Martin Nash <m-nash@unext.jp>
2026-04-28 22:21:17 +0000

    update patas sript

--- run_on_patas.condor.bat
+++ run_on_patas.condor.bat
@@ -8,10 +8,12 @@ getenv     = true
 notification = never
 transfer_executable = false
 request_memory = 8192
+request_GPUs = 1                                                                                     
+Requirements = (Machine == "patas-gn3.ling.washington.edu")
 
 queue model, name, peft from (
-  GanjinZero/biobart-v2-base,    biobart-base-lora,   --use-peft
-  GanjinZero/biobart-v2-large,   biobart-large-lora,  --use-peft
-  facebook/bart-base,            bart-base,           --use-peft
-  facebook/bart-large,           bart-large,          --use-peft
+  GanjinZero/biobart-v2-base,    gpu-biobart-base-lora,   --use-peft
+  GanjinZero/biobart-v2-large,   gpu-biobart-large-lora,  --use-peft
+  facebook/bart-base,            gpu-bart-base-lora,      --use-peft
+  facebook/bart-large,           gpu-bart-large-lora,     --use-peft
 )
