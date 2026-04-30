f2b382cc82d34d0ae3b4633c79d946b39d6e4a32
Martin Nash <m-nash@unext.jp>
2026-04-30 07:24:52 +0000

    debug inference file

--- inference.sh
+++ inference.sh
@@ -1,8 +1,19 @@
 #!/bin/sh
 
+echo "Host: $(hostname)"                                                                                                                                          
+echo "User: $(whoami)"                                                                                                                                            
+echo "Working dir: $(pwd)"                                                                                                                                        
+echo "HF_HOME: $HF_HOME"                                                                                                                                          
+echo "HF_HUB_CACHE: $HF_HUB_CACHE"                                                                                                                                
+
+
+
 # Set up environment
 source ~/miniconda3/etc/profile.d/conda.sh
 conda activate medjargone-gpu
+
+echo "Python: $(which python)"                                                                                                                                    
+
 
 # Run code
 python -m src.run_inference "$@"
