9e714e1944322d6330d351310ef752125e2a9ce8
Martin Nash <m-nash@unext.jp>
2026-04-30 08:43:42 +0000

    update env

--- inference.sh
+++ inference.sh
@@ -1,20 +1,8 @@
 #!/bin/sh
 
-echo "Host: $(hostname)"                                                                                                                                          
-echo "User: $(whoami)"                                                                                                                                            
-echo "Working dir: $(pwd)"                                                                                                                                        
-echo "HF_HOME: $HF_HOME"                                                                                                                                          
-echo "HF_HUB_CACHE: $HF_HUB_CACHE"                                                                                                                                
-
-
-
 # Set up environment
 source ~/miniconda3/etc/profile.d/conda.sh
 conda activate medjargone-gpu
-
-echo "Python: $(which python)"                                                                                                                                    
-
-export HF_HUB_OFFLINE=1
 
 # Run code
 python -m src.run_inference "$@"
