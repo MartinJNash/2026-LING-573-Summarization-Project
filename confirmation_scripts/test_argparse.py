a7a005c9e7969b53011c735f32c8ddd56298611b
Martin Nash <m-nash@unext.jp>
2026-05-30 03:51:02 +0000

    deleted confirmation scripts

--- confirmation_scripts/test_argparse.py
+++
@@ -1,7 +0,0 @@
-import argparse
-
-parser = argparse.ArgumentParser()
-parser.add_argument("--use-peft", action="store_true", default=False)
-args = parser.parse_args()
-
-print(f"use_peft: {args.use_peft}")
